d62f4c4961acf2330027dcadcae1473ba602274f
“Cory <coryurton@gmail.com>
2022-06-27 12:10:25 -0500

    feat: improved format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter some words: ");
+        System.out.println("Please enter something: ");
         String userInput = sc.nextLine();
 
         boolean isNum;
 
         if (!StringUtils.isNumeric(userInput)) {
-            System.out.println("that is not a number");
-            System.out.println("Look at this- your words with swapped casing: " + StringUtils.swapCase(userInput));
-            System.out.println("Heres another trick- your input REVERSED: " + StringUtils.reverse(userInput));
-        } else {
+            System.out.println("You Entered: \"" + userInput + "\"");
+            System.out.println("\"" + userInput + "\"" + " is not a number");
+            System.out.println("Flipped case: " + StringUtils.swapCase(userInput));
+            System.out.println("REVERSED case: " + StringUtils.reverse(userInput));
+        } else if (StringUtils.isNumeric(userInput)){
             System.out.println("you entered a number");
         }
 
